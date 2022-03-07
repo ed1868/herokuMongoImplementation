8c4d8e7cd447ff996bdbe0b8ed7b93c83c8a0480
ed1868 <ruizeduardo21@gmail.com>
2022-03-06 19:52:25 -0500

    mapbox token

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,6 +1,6 @@
 const Campground = require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
+const mapBoxToken = "pk.eyJ1IjoiaHdlazIxIiwiYSI6ImNsMGZ6aDR0cjB2cTIzY2s5a3l2Z3k5bTcifQ.mEFeoc4xR89F0BKWOdA5MA";
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
