9169754d717466c536bf4e434242f6ffd29625e1
ed1868 <ruizeduardo21@gmail.com>
2022-03-07 15:50:09 -0500

    added db url

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = "mongodb+srv://hwek21:foo@cluster0.ym2sz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
